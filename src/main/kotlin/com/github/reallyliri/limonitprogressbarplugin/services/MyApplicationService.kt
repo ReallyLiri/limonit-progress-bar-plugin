4ad80b4bf464249a3c2137428be8733309cfde6e
GitHub Action <action@github.com>
2022-06-16 09:28:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/reallyliri/limonitprogressbarplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.reallyliri.limonitprogressbarplugin.services
+
+import com.github.reallyliri.limonitprogressbarplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
