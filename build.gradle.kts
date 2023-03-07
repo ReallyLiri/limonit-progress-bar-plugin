49a381aad0824653aa3469995ea076db50913766
ReallyLiri <liri@diversion.dev>
2023-03-07 10:07:59 +0200

    Support version 231

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     // Java support
     id("java")
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.intellij") version "1.13.1"
 }
 
 group = properties("pluginGroup")
